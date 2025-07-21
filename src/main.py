427a2c14b1b31d0ef8215c4c3a577443ac3e4249
Malix <alixbrunetcontact@gmail.com>
2025-07-21 15:01:04 +0000

    enhance: indentation

--- src/main.py
+++ src/main.py
@@ -4,35 +4,35 @@ from datetime import datetime
 sense = SenseHat()
 
 def get_sense_data():
-    sense_data = []
-    # Get environmental data
-    sense_data.append(sense.get_temperature())
-    sense_data.append(sense.get_pressure())
-    sense_data.append(sense.get_humidity())
-    # Get orientation data
-    orientation = sense.get_orientation()
-    sense_data.append(orientation["yaw"])
-    sense_data.append(orientation["pitch"])
-    sense_data.append(orientation["roll"])
-    # Get compass data
-    mag = sense.get_compass_raw()
-    sense_data.append(mag["x"])
-    sense_data.append(mag["y"])
-    sense_data.append(mag["z"])
-    # Get accelerometer data
-    acc = sense.get_accelerometer_raw()
-    sense_data.append(acc["x"])
-    sense_data.append(acc["y"])
-    sense_data.append(acc["z"])
-    #Get gyroscope data
-    gyro = sense.get_gyroscope_raw()
-    sense_data.append(gyro["x"])
-    sense_data.append(gyro["y"])
-    sense_data.append(gyro["z"])
-    # Get the date and time
-    sense_data.append(datetime.now())
+	sense_data = []
+	# Get environmental data
+	sense_data.append(sense.get_temperature())
+	sense_data.append(sense.get_pressure())
+	sense_data.append(sense.get_humidity())
+	# Get orientation data
+	orientation = sense.get_orientation()
+	sense_data.append(orientation["yaw"])
+	sense_data.append(orientation["pitch"])
+	sense_data.append(orientation["roll"])
+	# Get compass data
+	mag = sense.get_compass_raw()
+	sense_data.append(mag["x"])
+	sense_data.append(mag["y"])
+	sense_data.append(mag["z"])
+	# Get accelerometer data
+	acc = sense.get_accelerometer_raw()
+	sense_data.append(acc["x"])
+	sense_data.append(acc["y"])
+	sense_data.append(acc["z"])
+	#Get gyroscope data
+	gyro = sense.get_gyroscope_raw()
+	sense_data.append(gyro["x"])
+	sense_data.append(gyro["y"])
+	sense_data.append(gyro["z"])
+	# Get the date and time
+	sense_data.append(datetime.now())
 
-    return sense_data
+	return sense_data
 
 while True:
 	print(get_sense_data())
